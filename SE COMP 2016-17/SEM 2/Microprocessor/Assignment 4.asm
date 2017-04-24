33617dce7239fc24f2230a3db8bfe7be6f2f9002
AshV1703 <ashwinnitinvaidya@gmail.com>
2017-04-24 11:30:09 +0530

    Bugfixes

--- SE COMP 2016-17/SEM 2/Microprocessor/Assignment 4.asm
+++ SE COMP 2016-17/SEM 2/Microprocessor/Assignment 4.asm
@@ -13,20 +13,20 @@ msglen equ $-msg
 msg1 db "Enter Multiplier: "
 msg1len equ $-msg1
 msg2 db "1. By repeated addition",10
-msg2len equ $-msg2
+;msg2len equ $-msg2
 msg3 db "2. By shift add method",10
-msg3len equ $-msg3
+;msg3len equ $-msg3
 msg4 db "3. EXIT",10
-msg4len equ $-msg4
+msg4len equ $-msg2
 
 
 
 section .bss
 
-inp resb 2 ;	 	For inputing options
-multiplier resb 1 ; 	For inputing Multiplier
-multiplicand resb 1; 	For inputing Multiplicand
-result resb 4; 
+inp resb 2 ;	 	For inputting options
+multiplier resb 1 ; 	For inputting Multiplier
+multiplicand resb 1; 	For inputting Multiplicand
+result resb 4;
 temp resb 3
 
 section .text
@@ -34,96 +34,138 @@ section .text
 global _start
 
 _start:
-	io 1,1,msg,msglen
-	io 0,0,temp,3
-	mov esi,multiplicand
-	call pack
-	io 1,1,msg1,msg1len
-	io 0,0,temp,3
-	mov esi,multiplier
-	call pack
 
 
-print:
-	io 1,1,msg2,msg2len
-	io 1,1,msg3,msg3len
-	io 1,1,msg4,msg4len
+
+	
+	call pack
+mov esi,multiplicand
+mov byte[esi],bl
+
+	;io 1,1,msg1,msg1len
+	;io 0,0,temp,3
+	call pack
+mov esi,multiplier
+mov byte[esi],bl
+
+	;io 1,1,msg2,msg4len
+	;io 1,1,msg3,msg3len
+	;io 1,1,msg4,msg4len
+	;io 0,0,inp,2
+		print:
+	io 1,1,msg2,msg4len
 	io 0,0,inp,2
-	
-	cmp byte[inp],31h
+
+	mov al,byte[inp]
+	cmp al,31h
 	je BY_ADD
-	
-	cmp byte[inp],32h
+
+	cmp al,32h
 	je BY_SHIFT
 
-	cmp byte[inp],33h
+	cmp al,33h
 	je EXIT
 BY_ADD:
 call ByAdd
+call display
 jmp print
 BY_SHIFT:
 call ByShift
+call display
 jmp print
+EXIT:
+io 60,0,0,0
+
 ByAdd:
 	mov ax,0
 	mov bl,0
+  	mov cx,0
 	mov al,byte[multiplicand]
 	mov bl,byte[multiplier]
-loop:	add byte[reslut],al
+loop:	
+	add cx,ax
 	dec bl
 	jnz loop
-	
-	call display
+
+  	mov word[result],cx
 ret
 
 ByShift:
+mov al,byte[multiplicand]
+	mov bl,byte[multiplier]
+	mov cx,0
+	mov dx,8H
+	
+	up2:
+		shl cx,1
+		shl al,1        
+		jnc ddo
+		add cx,bx
+		ddo:
+			dec dx
+			jnz up2
+	mov word[result],cx
 ret
 
 pack:
+	io 1,1,msg,msglen
+	io 0,0,temp,3
 	mov edi,temp
-	mov ax,0
+	
 	mov bx,0; result
-up:	mov al,byte[edi]
-	cmp al,0Ah
+up:
+	mov ax,0
+    mov al,byte[edi]
+	cmp al,0AH
 	je cmpl
-	cmp al,39h
+	cmp al,39H
 	jbe convert
-	sub al,07h
+	sub al,07H
 convert:
-	sub al,30h
-	and al,000Fh
+	sub al,30H
+	and al,000fH
 	rol bx,4
 	add bx,ax
 	inc edi
 	jmp up
-	
+
 cmpl:
-mov word[esi],bx
+
 ret
 
 display:
-	mov dx,0
-	mov cx,word[result]
-up1:	rol cx,4
-	mov [result],cx
-	and cx, 0fh
-	cmp cx,09h
+	mov bp,4
+	mov ax,word[result]
+up1:	
+	rol ax,4
+	mov word[result],ax
+	and al, 0FH
+	cmp al,09H
 	jbe down
-	add cx,30h
-down:	add cx,37h
-	mov [temp],al
+	add al,07H
+down:	
+	add al,30H
+	mov byte[temp],al
 	io 1,1,temp,1
-	inc dx
-	mov cx,[result]
-	jmp up1
+  	mov ax,word[result]
+	dec bp
+  jnz up1
+
+
 ret
-
-EXIT:
-io 60,0,0,0
-
-
-
-
-
-
+-----------OUTPUT-------------
+Enter Multiplicand: FF
+Enter Multiplicand: FF
+1. By repeated addition
+2. By shift add method
+3. EXIT
+1
+FE011. By repeated addition
+2. By shift add method
+3. EXIT
+2
+FE011. By repeated addition
+2. By shift add method
+3. EXIT
+3
 
